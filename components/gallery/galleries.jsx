78d27121d489f515445ca8c0600c8e46bb1f1c44
mocilnikcarlos <mocilnik.web@gmail.com>
2024-10-12 21:12:26 -0300

    fix(Root): fix json

--- components/gallery/galleries.jsx
+++ components/gallery/galleries.jsx
@@ -1,6 +1,6 @@
 import style from "./gallery.module.scss";
 import ProjectCard from "./ProjectCard";
-import projectsData from "@/data/projects.json";
+import projectsData from "../data/projects.json";
 
 const GalleryProjects = () => {
   return (
