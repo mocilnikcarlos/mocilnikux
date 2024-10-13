c36fa32e3473f4f27c24a64c10f015db21413708
mocilnikcarlos <mocilnik.web@gmail.com>
2024-10-13 13:08:27 -0300

    feat(Root): create content details project

--- app/projects/projectdetails/components/section.jsx
+++ app/projects/projectdetails/components/section.jsx
@@ -1,18 +1,43 @@
-import FlagArgentine from "@/components/svg/flagargentine";
 import DetailsMyJob from "./detailsmyjob";
 import NavigationsDetailsProjects from "./navigationsdetailsprojects";
 import style from "./section.module.scss";
+import ProblemResumeProject from "./problemconclusion";
 
-const SectionDetailsProjects = () => {
+const SectionDetailsProjects = ({
+  design,
+  date,
+  problemData = [], // Valor predeterminado como array vacío
+  summaryData = [], // Valor predeterminado como array vacío
+  svg,
+  children,
+}) => {
   return (
     <section className={style.section}>
       <div className={style.contentsection}>
         <NavigationsDetailsProjects />
-        <DetailsMyJob
-          design={"Diseño UX/UI - Product Designer"}
-          date={"2023-2024"}
-          svg={<FlagArgentine />}
-        />
+        <DetailsMyJob design={design} date={date} svg={svg} />
+
+        {/* Mapeo para el problema */}
+        {problemData.map((item, index) => (
+          <ProblemResumeProject
+            key={`problem-${index}`}
+            title={item.title}
+            body={item.body}
+            imageName={item.imageName}
+          />
+        ))}
+
+        <div className={style.contentchildren}>{children}</div>
+
+        {/* Mapeo para el resumen */}
+        {summaryData.map((item, index) => (
+          <ProblemResumeProject
+            key={`summary-${index}`}
+            title={item.title}
+            body={item.body}
+            imageName={item.imageName}
+          />
+        ))}
       </div>
     </section>
   );
--- app/projects/projectdetails/jukeapp.jsx
+++ app/projects/projectdetails/jukeapp.jsx
@@ -1,7 +1,45 @@
+import FlagArgentine from "@/components/svg/flagargentine";
 import SectionDetailsProjects from "./components/section";
+import TitleSection from "./components/titlesection";
+import ImageProject from "./components/imageproject";
 
-const JukeApp = () => {
-  return <SectionDetailsProjects>Hola, desde jukeapp</SectionDetailsProjects>;
+const Jukeapp = () => {
+  const problemData = [
+    {
+      title: "Problema",
+      body: "Textos sobre el problema.",
+      imageName: "problemjuke",
+    },
+    // Puedes agregar más objetos si es necesario
+  ];
+
+  const summaryData = [
+    {
+      title: "Resumen",
+      body: "Textos sobre el resumen.",
+      imageName: "problemjuke",
+    },
+    // Puedes agregar más objetos si es necesario
+  ];
+
+  return (
+    <SectionDetailsProjects
+      design="Diseño UX/UI - Product Designer"
+      date="2023-2024"
+      problemData={problemData}
+      summaryData={summaryData}
+      svg={<FlagArgentine />}
+    >
+      <TitleSection
+        title={"User persona"}
+        body={
+          "El éxito del vendedor y del comprador es fundamental para impulsar la logística. Asi que identificados los problemas, me centro en los tipos de usuarios"
+        }
+      />
+      <ImageProject imageName={"userlaurajukeapp"} />
+      <ImageProject imageName={"usermartinjukeapp"} />
+    </SectionDetailsProjects>
+  );
 };
 
-export default JukeApp;
+export default Jukeapp;
