ba1d1c1f0ee366a5433dd00c793ba7c5297f26f8
mocilnikcarlos <mocilnik.web@gmail.com>
2024-11-01 17:58:00 -0300

    fix(Root): fix 404

--- app/not-found.js
+++ app/not-found.js
@@ -1,35 +1,19 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import style from "./not-found.module.scss";
-import Link from "next/link";
+import styles from "./not-found.module.scss";
+import { ButtonNav } from "@/components/button/buttons";
 
 const NameProjectFail = () => {
   const pathname = usePathname();
   const param = pathname.split("/").pop();
 
   return (
-    <div className={style.contentnamefail}>
-      <h2 className={style.title}>
-        ¡Wow ... <span className={style.span}>{param}</span> es un gran nombre
-        para un proyecto!
-      </h2>
-      <p className={style.text}>
-        ¿Que te parece si trabajamos juntos y lo diseñamos?
-      </p>
-      <div className={style.buttons}>
-        <a
-          href="https://calendly.com/catquantum/exploremos-juntos-como-puedo-contribuir-a-tu-equipo"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={style.btnspeack}
-        >
-          Hablemos 😉
-        </a>
-        <Link href={"/"} className={style.btnback}>
-          Mejor otro dia 🙄
-        </Link>
-      </div>
+    <div className={styles.contentnamefail}>
+      <h3 className={styles.title}>
+        La página <span className={styles.span}>{param}</span> no existe.
+      </h3>
+      <ButtonNav href={"/"} label={"Volver al portafolio"} />
     </div>
   );
 };
